f01c74ad85d65de65479b84948f3d21497f5da1b
Dduhalde <dduhalde@hotmail.com>
2023-11-12 23:47:50 -0300

    Cambio navbar, duplicacion notificaciones en empleo

--- proyecto-diseno-software-20232/src/Components/Navbar.js
+++ proyecto-diseno-software-20232/src/Components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
     }
 
     return (
-    <nav class="navbar navbar-expand-lg bg-info-subtle" style={navbarStyle}>
+    <nav class="navbar navbar-expand-lg bg-primary" style={navbarStyle}>
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -23,30 +23,30 @@ function Navbar() {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item" style={navbarItemStyle}>
-            <a class="nav-link" aria-current="page" href="/feed"> <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <a class="nav-link text-light fw-semibold" aria-current="page" href="/feed"> <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
   <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
   <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-</svg> Inicio</a>
+</svg>  Inicio</a>
             </li>
             <li class="nav-item" style={navbarItemStyle}>
-            <a class="nav-link" href="/jobs"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-briefcase-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <a class="nav-link text-light fw-semibold" href="/jobs"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-briefcase-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M3 9a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9z" />
             <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
-            </svg>Empleos</a>
+            </svg>  Empleos</a>
             </li>
             <li class="nav-item" style={navbarItemStyle}>
-            <a class="nav-link" href="/notifications"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bell" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <a class="nav-link text-light fw-semibold" href="/notifications"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bell" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
   <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
-</svg> Notificaciones</a>
+</svg>  Notificaciones</a>
             </li>
             <li class="nav-item dropdown" style={navbarItemStyle}>
-            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <a class="nav-link dropdown-toggle text-light fw-semibold" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
   <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
@@ -63,7 +63,7 @@ function Navbar() {
         </ul>
         <form class="d-flex" role="search" style={navbarItemStyle}>
             <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"></input>
-            <button class="btn btn-outline-success" type="submit">Buscar</button>
+            <button class="btn btn-outline-light btn-lg" type="submit">Buscar</button>
         </form>
         </div>
     </div>
--- proyecto-diseno-software-20232/src/Pages/Jobs.js
+++ proyecto-diseno-software-20232/src/Pages/Jobs.js
@@ -1,15 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Notification from '../Components/Job';
+import axios from 'axios';
 
 function Jobs() {
 
+  // Utilización de la API randomuser.me
+
+  const post_amount = Math.floor(Math.random() * 11);
+  const apiNamesUrl = `https://randomuser.me/api/?results=${post_amount}&nat=us`;
+  const apiDatesUrl = `https://api.lrs.org/random-date-generator?num_dates=${post_amount}`;
+
+
+  // Obtención de nombres y apellidos
+  const [nombreUsuarios, setNombreUsuarios] = useState([]);
+
+  useEffect( () => {
+
+      axios.get(apiNamesUrl).then((response) => {  
+        setNombreUsuarios(response.data.results)
+      }).catch((error) => {
+          console.error('Error');
+      });
+
+  },[]);
+  // Obtención de fechas
+  const [fechas, setFechas] = useState([]);
+
+  useEffect( () => {
+
+      axios.get(apiDatesUrl).then((response) => {  
+        setFechas(response.data.data)
+      }).catch((error) => {
+          console.error('Error');
+      });
+
+  },[]);
+
+  const fechasArray = Object.keys(fechas)
+  const listaInvertida = [...fechasArray].reverse();
+
     return (
-        <h1>Hello World, Jobs</h1>
+      <main className='bg-secondary-subtle'>
+        <div className='container p-3'>
+              {/* Crear las publicaciones del feed */}
+              {nombreUsuarios.length > 0 ? (
+                              nombreUsuarios.map((usuario, index) => (
+                                <Notification key={index} nombrePersona={usuario.name.first} apellidoPersona={usuario.name.last} fotoPerfilPersona={usuario.picture.medium} fechaNoti={listaInvertida[index]}/>
+                              ))
+              ) : (
+                <p>Cargando notificaciones...</p>
+              )}
+        </div>
+      </main>
     );
 }
 
 export default Jobs;
-
-
-
