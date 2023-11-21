5f1df0a24d511c18e88484b9878660e9f2b7c0b4
Dduhalde <dduhalde@hotmail.com>
2023-11-20 23:09:00 -0300

    Update Register.js

--- proyecto-diseno-software-20232/src/Pages/Register.js
+++ proyecto-diseno-software-20232/src/Pages/Register.js
@@ -17,42 +17,79 @@ function Register() {
     }
 
     return (
-        <main className="bg-secondary" style={registerFormStyle}>
-            <div id="formulario-registro" className="col-md-7 mx-auto">
-            <h1 className="text-center p-3">Bienvenido a Mendelevium</h1>
-                {/* Formulario de registro en la plataforma */}
-                <form className="text-center text-light p-5" style={specificLabelStyling}>
-                    <div className="row">
-                        <div className="mb-5 col">
-                            <label for="inputNombres" class="form-label">Nombres</label>
-                            <input type="text" class="form-control border-4 border-dark" name="nombres" placeholder="Nombres" required></input>
-                        </div>
-                        <div className="mb-5 col">
-                            <label for="inputApellidos" class="form-label">Apellidos</label>
-                            <input type="text" class="form-control border-4 border-dark" name="apellidos" placeholder="Apellidos" required></input>
-                        </div>
-                        <div className="mb-5">
-                            <label for="inputBirthday" class="form-label">Fecha de nacimiento</label>
-                            <input type="date" class="form-control border-4 border-dark" name="fecha_nacimiento" required></input>
-                        </div>
-                        <div className="mb-5 col">
-                            <label for="inputCorreo" class="form-label">Correo electrónico</label>
-                            <input type="email" class="form-control border-4 border-dark" name="correo" placeholder="ejemplo@dirección.cl" required></input>
-                        </div>
-                        <div className="mb-5 col">
-                            <label for="inputTelefono" class="form-label">Teléfono de contacto</label>
-                            <input type="tel" class="form-control border-4 border-dark" name="telefono" placeholder="(+56 9) 1234 5678" maxlength="8" required></input>
-                        </div>
-                        <div className="mb-5">
-                            <label for="inputProfesion" class="form-label">Profesión</label>
-                            <input type="text" class="form-control border-4 border-dark" name="profesion" placeholder="Profesión / Oficio" required></input>
-                        </div>
-                    </div>
-                    {/* Botón de envío del formulario */}
-                    <div>
-                        <button type="submit" className="btn btn-primary border-3 border-black">Registrarme</button>
-                    </div>
-                </form>
+        <main className="d-flex align-items-center justify-content-center bg-secondary-subtle">
+            <div class="container pt-4">
+            <h2 class="mb-4 text-center">Formulario de Registro</h2>
+            <form>
+                <div class="mb-3">
+                <label for="nombres" class="form-label">Nombres:</label>
+                <input type="text" class="form-control" id="nombres" required></input>
+                </div>
+                <div class="mb-3">
+                <label for="apellidos" class="form-label">Apellidos:</label>
+                <input type="text" class="form-control" id="apellidos" required></input>
+                </div>
+                <div class="mb-3">
+                <label for="correo" class="form-label">Correo:</label>
+                <input type="email" class="form-control" id="correo" required></input>
+                </div>
+                <div class="mb-3">
+                <label for="telefono" class="form-label">Teléfono:</label>
+                <input type="tel" class="form-control" id="telefono" required></input>
+                </div>
+                <div class="mb-3">
+                <label for="fechaNacimiento" class="form-label">Fecha de Nacimiento:</label>
+                <input type="date" class="form-control" id="fechaNacimiento" required></input>
+                </div>
+                <div class="mb-3">
+                <label for="comuna" class="form-label">Comuna:</label>
+                <input type="text" class="form-control" id="comuna" required></input>
+                </div>
+                <div class="mb-3">
+                <label for="region" class="form-label">Región:</label>
+                <input type="text" class="form-control" id="region" required></input>
+                </div>
+                <div class="mb-3">
+                <label for="profesion" class="form-label">Profesión:</label>
+                        <select id="profesion" name="profesion" class="form-select">
+                            <option value="Albañil">Albañil</option>
+                            <option value="Carpintero">Carpintero</option>
+                            <option value="Electricista">Electricista</option>
+                            <option value="Fontanero">Fontanero</option>
+                            <option value="Pintor">Pintor</option>
+                            <option value="Soldador">Soldador</option>
+                            <option value="Techador">Techador</option>
+                            <option value="Jardinero/Landscaper">Jardinero/Landscaper</option>
+                            <option value="Trabajador de demolición">Trabajador de demolición</option>
+                            <option value="Operador de maquinaria pesada">Operador de maquinaria pesada</option>
+                            <option value="Ayudante de construcción">Ayudante de construcción</option>
+                            <option value="Operador de montacargas">Operador de montacargas</option>
+                            <option value="Encargado de limpieza y mantenimiento">Encargado de limpieza y mantenimiento</option>
+                            <option value="Operador de herramientas manuales">Operador de herramientas manuales</option>
+                            <option value="Instalador de aislamiento">Instalador de aislamiento</option>
+                            <option value="Asistente de carpintería">Asistente de carpintería</option>
+                            <option value="Operador de grúa móvil">Operador de grúa móvil</option>
+                            <option value="Operador de excavadora">Operador de excavadora</option>
+                            <option value="Armador de estructuras de acero">Armador de estructuras de acero</option>
+                            <option value="Instalador de suelos">Instalador de suelos</option>
+                            <option value="Operario de encofrado">Operario de encofrado</option>
+                            <option value="Conserje">Conserje</option>
+                            <option value="Operador de compactadora">Operador de compactadora</option>
+                            <option value="Operador de miniexcavadora">Operador de miniexcavadora</option>
+                            <option value="Instalador de tuberías">Instalador de tuberías</option>
+                            <option value="Ayudante de electricista">Ayudante de electricista</option>
+                            <option value="Auxiliar de plomería">Auxiliar de plomería</option>
+                            <option value="Auxiliar de pintura">Auxiliar de pintura</option>
+                        </select>
+                </div>
+                <div class="form-check mb-3">
+                <input type="checkbox" class="form-check-input" id="aceptaTerminos" required></input>
+                <label class="form-check-label" for="aceptaTerminos">Acepto los términos de servicio</label>
+                </div>
+                <div class="text-center pb-4">
+                <button type="submit" class="btn btn-primary">Registrar</button>
+                </div>
+            </form>
             </div>
         </main>
     )
