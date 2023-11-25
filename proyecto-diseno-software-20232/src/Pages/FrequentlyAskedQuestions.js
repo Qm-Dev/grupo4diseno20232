f8c241c26b7038eccdb57bc18d18824f91a94382
Dduhalde <dduhalde@hotmail.com>
2023-11-24 23:52:11 -0300

    FAQ nuevo

--- proyecto-diseno-software-20232/src/Pages/FrequentlyAskedQuestions.js
+++ proyecto-diseno-software-20232/src/Pages/FrequentlyAskedQuestions.js
@@ -2,58 +2,138 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function FrequentlyAskedQuestions() {
-
-    // Estilo del titulo
-    const titleFaq = {
-        textAlign: 'center',
-        paddingTop: '0.75rem 1.5rem',
-        paddingBottom: '0.5rem',
-        marginLeft: '1.5rem',
-        marginRight: '1.5rem'
-    }
-
-    // Estilo de los párrafos
-    const paragraphFaq = {
-        textAlign: 'justify',
-        paddingTop: '0.75rem',
-        paddingBottom: '0.5rem',
-        marginLeft: '1.5rem',
-        marginRight: '1.5rem'
-    }
-
     return (
-        <main>
-            <h1 class="pt-4" style={titleFaq}>Frequently Asked Questions</h1>
-            <h2 style={paragraphFaq}>Pregunta 1</h2>
-            <p style={paragraphFaq}>Sed fringilla dolor eros, sed congue libero faucibus a. Nullam viverra
-            velit ac pellentesque hendrerit. Aenean sed mi quis mi rutrum dapibus quis ac lacus. Sed velit lectus,
-            bibendum a elit vel, commodo facilisis nibh. Integer nec est nunc. Nam vel sollicitudin lectus. Mauris
-            molestie faucibus augue, blandit rhoncus sapien egestas non. Pellentesque ultrices ipsum rhoncus enim
-            dictum feugiat. Quisque tempor vel est ac placerat.</p>
-            <h2 style={paragraphFaq}>Pregunta 2</h2>
-            <p style={paragraphFaq}>Sed fringilla dolor eros, sed congue libero faucibus a. Nullam viverra
-            velit ac pellentesque hendrerit. Aenean sed mi quis mi rutrum dapibus quis ac lacus. Sed velit lectus,
-            bibendum a elit vel, commodo facilisis nibh. Integer nec est nunc. Nam vel sollicitudin lectus. Mauris
-            molestie faucibus augue, blandit rhoncus sapien egestas non. Pellentesque ultrices ipsum rhoncus enim
-            dictum feugiat. Quisque tempor vel est ac placerat.</p>
-            <h2 style={paragraphFaq}>Pregunta 3</h2>
-            <p style={paragraphFaq}>Sed fringilla dolor eros, sed congue libero faucibus a. Nullam viverra
-            velit ac pellentesque hendrerit. Aenean sed mi quis mi rutrum dapibus quis ac lacus. Sed velit lectus,
-            bibendum a elit vel, commodo facilisis nibh. Integer nec est nunc. Nam vel sollicitudin lectus. Mauris
-            molestie faucibus augue, blandit rhoncus sapien egestas non. Pellentesque ultrices ipsum rhoncus enim
-            dictum feugiat. Quisque tempor vel est ac placerat.</p>
-            <h2 style={paragraphFaq}>Pregunta 4</h2>
-            <p style={paragraphFaq}>Sed fringilla dolor eros, sed congue libero faucibus a. Nullam viverra
-            velit ac pellentesque hendrerit. Aenean sed mi quis mi rutrum dapibus quis ac lacus. Sed velit lectus,
-            bibendum a elit vel, commodo facilisis nibh. Integer nec est nunc. Nam vel sollicitudin lectus. Mauris
-            molestie faucibus augue, blandit rhoncus sapien egestas non. Pellentesque ultrices ipsum rhoncus enim
-            dictum feugiat. Quisque tempor vel est ac placerat.</p>
-            <h2 style={paragraphFaq}>Pregunta 5</h2>
-            <p style={paragraphFaq}>Sed fringilla dolor eros, sed congue libero faucibus a. Nullam viverra
-            velit ac pellentesque hendrerit. Aenean sed mi quis mi rutrum dapibus quis ac lacus. Sed velit lectus,
-            bibendum a elit vel, commodo facilisis nibh. Integer nec est nunc. Nam vel sollicitudin lectus. Mauris
-            molestie faucibus augue, blandit rhoncus sapien egestas non. Pellentesque ultrices ipsum rhoncus enim
-            dictum feugiat. Quisque tempor vel est ac placerat.</p>
+        <main className='bg-secondary-subtle'>
+            <div class="container py-5">
+    <div class="card mx-auto">
+        <h1 class="card-header text-uppercase fw-bold text-center">Preguntas Frecuentes</h1>
+        <div class="card-body">
+            <div class="accordion" id="faqAccordion">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            ¿Cómo puedo registrarme en el sitio?
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Puedes registrarte haciendo clic en el botón "Registrarse" en la página de inicio y completando el formulario.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            ¿Cómo restablezco mi contraseña?
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Para restablecer tu contraseña, ve a la página de inicio de sesión y haz clic en "¿Olvidaste tu contraseña?" Sigue las instrucciones proporcionadas.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            ¿Cómo puedo buscar empleos disponibles?
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Utiliza la barra de búsqueda en la página de empleos para ingresar palabras clave y filtrar los resultados según tus preferencias.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingFour">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            ¿Cómo puedo aplicar a un trabajo?
+                        </button>
+                    </h2>
+                    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Después de encontrar un trabajo que te interese, haz clic en "Aplicar ahora" y sigue las instrucciones para completar y enviar tu solicitud.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingFive">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                            ¿Cómo puedo eliminar mi cuenta?
+                        </button>
+                    </h2>
+                    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Para eliminar tu cuenta, inicia sesión, ve a la configuración de la cuenta y busca la opción para eliminar cuenta. Sigue las instrucciones proporcionadas.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingSix">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                            ¿Cómo puedo actualizar mi información de perfil?
+                        </button>
+                    </h2>
+                    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Inicia sesión en tu cuenta y ve a la sección de "Perfil". Allí encontrarás opciones para editar y actualizar tu información personal.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingSeven">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                            ¿Qué debo hacer si olvidé mi nombre de usuario?
+                        </button>
+                    </h2>
+                    <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Si olvidaste tu nombre de usuario, intenta recuperarlo utilizando la opción de "¿Olvidaste tu nombre de usuario?" en la página de inicio de sesión.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingEight">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                            ¿Cómo puedo recibir notificaciones de nuevos empleos?
+                        </button>
+                    </h2>
+                    <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Asegúrate de haber activado las notificaciones en la configuración de tu cuenta. También puedes personalizar tus preferencias de notificación.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingNine">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                            ¿Cuánto tiempo tarda en procesarse una solicitud de trabajo?
+                        </button>
+                    </h2>
+                    <div id="collapseNine" class="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            El tiempo de procesamiento de una solicitud de trabajo puede variar. Algunos empleadores revisan las solicitudes de inmediato, mientras que otros pueden tardar más tiempo.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTen">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+                            ¿Cómo puedo cambiar mi contraseña?
+                        </button>
+                    </h2>
+                    <div id="collapseTen" class="accordion-collapse collapse" aria-labelledby="headingTen" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Ve a la sección de configuración de tu cuenta y busca la opción para cambiar la contraseña. Sigue las instrucciones proporcionadas para actualizar tu contraseña.
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
         </main>
     );
 }
